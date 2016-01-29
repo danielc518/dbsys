1040f48332e6283e71a238940b12e02350300387
danielc518 <farewell518@gmail.com>
2016-01-29 00:40:14 -0500

    Implemented q3.todo.sql

--- hw0/dbsys_hw0/sql/q3.todo.sql
+++ hw0/dbsys_hw0/sql/q3.todo.sql
@@ -8,4 +8,36 @@
 
 -- Student SQL code here:
 
-
+WITH total_quantity AS
+  (SELECT SUM(l_quantity) AS t_quantity,
+          n_nationkey,
+          n_name,
+          p_partkey,
+          p_name
+   FROM lineitem,
+        orders,
+        customer,
+        nation,
+        part
+   WHERE l_orderkey = o_orderkey
+     AND o_custkey = c_custkey
+     AND c_nationkey = n_nationkey
+     AND l_partkey = p_partkey
+   GROUP BY n_nationkey,
+            l_partkey
+   ORDER BY SUM(l_quantity))
+SELECT n_nationkey,
+       n_name,
+       p_partkey,
+       p_name,
+       t_quantity
+FROM total_quantity,
+  (SELECT MAX(t_quantity) AS max_quantity,
+          n_nationkey AS t_nationkey
+   FROM total_quantity
+   GROUP BY n_nationkey)
+WHERE t_quantity = max_quantity
+  AND n_nationkey = t_nationkey
+ORDER BY n_nationkey,
+         p_partkey ASC;
+		 
